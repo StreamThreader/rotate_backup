a8fcb7387f96b343f5a858b752cfd2f2507611b2
StreamThreader <admin@ifconfig.com.ua>
2024-10-07 21:45:03 +0300

    add test script

---
+++ fill-test_dir.sh
@@ -0,0 +1,24 @@
+#!/usr/local/bin/bash
+
+TARGETDIR="/home/sthreader/dev/rotate_backup/test_dir"
+
+if [ ! -d "$TARGETDIR" ]; then
+    echo "directory: $TARGETDIR not found, exit"
+    exit 1
+fi
+
+cd "$TARGETDIR"
+
+# clear test_dir
+for file in $(ls 1 "$TARGETDIR/"); do
+    rm "$TARGETDIR/$file"
+done
+
+# fill test_dir
+for year in 2024; do
+    for month in $(seq -w 01 12); do
+	for day in $(seq -w 01 30); do
+	    touch "$TARGETDIR/$year-$month-$day-v8.3-TW-ERP--01-00-d.dt"
+	done
+    done
+done
